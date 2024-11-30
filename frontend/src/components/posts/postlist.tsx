953db89aeb385d6a3888211837588ea97005078d
L4B0MB4 <metalfan65@gmail.com>
2024-11-30 13:55:19 +0100

    Enhance post list user data retrieval by including own user in the user list

--- frontend/src/components/posts/postlist.tsx
+++ frontend/src/components/posts/postlist.tsx
@@ -7,10 +7,14 @@ import { ContentCard } from "./contentcard";
 export const PostList = () => {
   const posts = useAppSelector((state) => {
     const posts = state.posts.posts;
+    let users = state.users.users;
+    if (state.users.ownUser) {
+      users = [...users, state.users.ownUser];
+    }
     const enahncedPosts = posts.map((post) => {
       return {
         ...post,
-        user: state.users.users.find((user) => user.id === post.user_id) ?? {
+        user: users.find((user) => user.id === post.user_id) ?? {
           display_name: "Unknown",
           id: "Unknown",
         },
